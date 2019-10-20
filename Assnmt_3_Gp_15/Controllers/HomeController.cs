da3ccc06ff50974a03487baab962fe9f2907cb49
nnkchem <nnkchem@gmail.com>
2019-10-20 19:39:59 -0400

    Add project files.

---
+++ Assnmt_3_Gp_15/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assnmt_3_Gp_15.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        //public IActionResult Index()
+        //{
+        //    int hour = DateTime.Now.Hour;
+        //    ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+        //    return View("myView");
+        //}
+
+        //public IActionResult Index()
+        //{
+        //    return View("Data");
+        //}
+
+
+
+        public IActionResult Index()
+        {
+            return View("main");
+        }
+
+        public IActionResult Orlando()
+        {
+            return View("Greater_Orlando");
+        }
+
+        public IActionResult Tampa()
+        {
+            return View("Greater_Tampa");
+        }
+
+        public IActionResult Miami()
+        {
+            return View("Greater_Miami");
+        }
+
+        public IActionResult AboutProject()
+        {
+            return View("About_Project");
+        }
+        
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //public IActionResult About()
+        //{
+        //    ViewData["Message"] = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public IActionResult Contact()
+        //{
+        //    ViewData["Message"] = "Your contact page.";
+
+        //    return View();
+        //}
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+    }
+}
---
+++ Assnmt_3_Gp_15/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Assnmt_3_Gp_15.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
